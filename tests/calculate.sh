7112038684e44e0c8652e79505eca8a710f0cea5
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2020-04-29 17:48:18 -0400

    Check for md5sum equal bams

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -9,7 +9,7 @@ qrsh -l h_vmem=8G -cwd -now n "\
 . /oicr/local/Modules/default/init/bash; \
 module load samtools 2>/dev/null; \
 find . -regex '.*\.bam$' \
-       -exec sh -c \" samtools flagstat {} | tr '\n' '\t'; echo \" \; \
+       -exec sh -c \" samtools flagstat {} | tr '\n' '\t'; echo ; printf "{}="; samtools view {} | md5sum \" \; \
 | sort | uniq | tr '\t' '\n'"
 
 # get a listing of the output files
